f80a7f0e0cfc19e491fc152c759a52b1dbaf1fcc
jcariaga <jay_jay_xray@yahoo.com>
2019-12-06 18:00:58 -0500

    updating screen 1

--- app/src/main/java/com/example/androidprojectchess20/MainActivity.java
+++ app/src/main/java/com/example/androidprojectchess20/MainActivity.java
@@ -1,8 +1,13 @@
+/**
+ * @author Group 20: Jonathan Lin (jkl130) & Jason Cariaga (jmc803)
+ * @version 1.0
+ */
 package com.example.androidprojectchess20;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void playermove(){}
+
+    public void undo(){}
+
+    public void draw(){}
+
+    public void resign(){}
+
+    public void AImove(){}
+
+    public void resetboard(){}
+
+    public void chooseGame(){}
+
+
+
 }
+
